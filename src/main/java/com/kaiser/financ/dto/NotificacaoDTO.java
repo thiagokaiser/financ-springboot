e811ab3714799ac9828533412a71c7efd16cf697
Thiago Kaiser <thiago.kaiser@hotmail.com>
2023-02-27 19:14:27 -0300

    configs iniciais docker

--- src/main/java/com/kaiser/financ/dto/NotificacaoDTO.java
+++ src/main/java/com/kaiser/financ/dto/NotificacaoDTO.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 
 import com.kaiser.financ.domain.Notificacao;
 
-@Entity
 public class NotificacaoDTO implements Serializable{
     private static final long serialVersionUID = 1L;
         
