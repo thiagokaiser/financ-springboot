70734c1143e61cd1c0036d66ba3302489aba20e4
Thiago Kaiser <thiago.kaiser@hotmail.com>
2023-06-09 08:33:20 -0300

    fix domain annotation

--- src/main/java/com/kaiser/financ/domain/Domain.java
+++ src/main/java/com/kaiser/financ/domain/Domain.java
@@ -1,15 +1,15 @@
 package com.kaiser.financ.domain;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 
-@Entity
+@MappedSuperclass
 public abstract class Domain implements Serializable {
   protected static final long serialVersionUID = 1L;
 
